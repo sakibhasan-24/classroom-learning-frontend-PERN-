8a93c3da74ac0ef728acff2001c2b6cd829a5521
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-27 22:00:20 +0600

    filter by department UI

--- src/pages/subjects/SubjectsLists.tsx
+++ src/pages/subjects/SubjectsLists.tsx
@@ -1,7 +1,10 @@
+
+import { CreateButton } from "@/components/refine-ui/buttons/create.tsx"
 import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb"
 import { ListView } from "@/components/refine-ui/views/list-view"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { DEPARTMENT_OPTION } from "@/constant"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@radix-ui/react-select"
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@radix-ui/react-select"
 import { Search } from "lucide-react"
 import { useState } from "react"
 
@@ -25,8 +28,27 @@ const SubjectsLists = () => {
                placeholder="search by name" className="sm:w-1/2 w-full p-2   outline-none" 
                value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)}/>
             </div>
-            <div className=" ">
-              <h1>Hello</h1>
+            <div className="w-full flex gap-2 mt-4 sm:w-auto">
+             <Select value={selectedDepartment || "all"} onValueChange={setSelectedDepartment}>
+              <SelectTrigger className="w-full border-2 border-gray-300 rounded-md p-2">
+                <SelectValue placeholder="Select  Department"/>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">
+                  All Departments
+                </SelectItem>
+                {
+                  DEPARTMENT_OPTION.map((dept)=>(
+                    <SelectItem key={dept.value} value={dept.value}>
+                      {dept.label}
+                    </SelectItem>
+                  ))
+                }
+              </SelectContent>
+             </Select>
+           {/* <CreateButton/> */}
+           {/* <button>Create</button> */}
+           <CreateButton/>
             </div>
           </div>
         </div>
