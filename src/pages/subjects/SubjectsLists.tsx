64a4ef8fe681a16aca216b6331def0cb6f64d287
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-28 21:06:00 +0600

    datatable showing using refine core(fun and debug)😊

--- src/pages/subjects/SubjectsLists.tsx
+++ src/pages/subjects/SubjectsLists.tsx
@@ -1,17 +1,66 @@
 
 import { CreateButton } from "@/components/refine-ui/buttons/create.tsx"
+import { DataTable } from "@/components/refine-ui/data-table/data-table"
+// import { DataTable } from "@/components/refine-ui/data-table/data-table"
 import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb"
 import { ListView } from "@/components/refine-ui/views/list-view"
+import { Badge } from "@/components/ui/badge"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { DEPARTMENT_OPTION } from "@/constant"
+import { Subject } from "@/types"
+import { useTable } from "@refinedev/react-table"
+
+import { ColumnDef } from "@tanstack/react-table"
+
 // import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@radix-ui/react-select"
 import { Search } from "lucide-react"
-import { useState } from "react"
+
+
+import { useMemo, useState } from "react"
 
 
 const SubjectsLists = () => {
   const [searchQuery,setSearchQuery]=useState("")
   const [selectedDepartment,setSelectedDepartment]=useState("all")
+  
+
+  const subjectTable=useTable<Subject>({
+    columns : useMemo<ColumnDef<Subject>[]>(
+    () => [
+      {
+        id: "code",
+        accessorKey: "code",
+        size:100,
+        header: () => <p className="ml-2">Code</p>,
+        cell: ({ getValue }) =>  (
+            <Badge className="px-2 py-1 rounded-md bg-neutral-800 text-gray-200">
+              {getValue<string>()}
+            </Badge>
+          )
+        
+      },
+      {
+        id:"name",
+        accessorKey: "name",
+        size:200,
+        header: () => <p className="ml-2">Name</p>,
+        cell: ({ getValue }) =>(
+          <span>{getValue<string>()}</span>
+        )
+      }
+    ],
+    []
+  )
+  ,
+    refineCoreProps:{
+      resource: "subject",
+       pagination:{
+      pageSize: 10,
+      mode:"server"
+    }
+    }
+  })
+  // console.log(subjectTable)
   return (
    <ListView>
     <Breadcrumb/>
@@ -46,12 +95,12 @@ const SubjectsLists = () => {
                 }
               </SelectContent>
              </Select>
-           {/* <CreateButton/> */}
-           {/* <button>Create</button> */}
-           <CreateButton/>
+                <CreateButton resource="subject"/>
             </div>
           </div>
         </div>
+        {/* <DataTable table={subjectTable}/> */}
+        <DataTable table={subjectTable}/>
       </div>
    </ListView>
   )
